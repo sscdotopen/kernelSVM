de36fa6e3dc732ccef802f69536a1438de0d3893
ssc <ssc.open@googlemail.com>
2016-01-22 19:27:47 +0100

    added convergence acceleration

--- src/main/scala/Svm/distributed/SparkDskl.scala
+++ src/main/scala/Svm/distributed/SparkDskl.scala
@@ -1,6 +1,7 @@
 package Svm.distributed
 
 import breeze.linalg._
+import breeze.numerics._
 import breeze.stats.distributions.Rand
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
@@ -44,6 +45,8 @@ object SparkDskl extends App {
 
     /* initialize coefficient vector */
     var α = DenseVector.rand(N, Rand.gaussian)
+    val G = DenseVector.ones[Double](N)
+
     val sc = instances.sparkContext
     
     try {
@@ -58,7 +61,7 @@ object SparkDskl extends App {
         /* broadcast dual coefficients */
         val α_broadcast = sc.broadcast(α)
 
-        val g = partitions
+        val (g, deltaGk) = partitions
           .map { partition =>
   
             /* slice out relevant part of coefficient vector */
@@ -66,8 +69,13 @@ object SparkDskl extends App {
   
             val rnPred = DistUtils.sampleNoSeed(partition.Y.length, numGradients)
             val rnexpand = DistUtils.sampleNoSeed(partition.Y.length, empiricalKernelMapWidth)
-            val gk_local = Dskl.gradient(partition.Y(rnPred), partition.X(::, rnPred), partition.X(::, rnexpand),
+
+            val gk_localAndGhat = gradientAndG(partition.Y(rnPred), partition.X(::, rnPred), partition.X(::, rnexpand),
               α_local(rnexpand).asInstanceOf[Vector[Double]], C)
+
+           //val gk_local = Dskl.gradient(partition.Y(rnPred), partition.X(::, rnPred), partition.X(::, rnexpand),
+           //  α_local(rnexpand).asInstanceOf[Vector[Double]], C)
+           //assert(norm((gk_local - gk_localAndGhat._1), 2) < 0.0001)
 
             /* compute training error on current partition */
             if (iteration % 3 == 0) {
@@ -83,15 +91,22 @@ object SparkDskl extends App {
             }
   
             /* correctly translate sample partition indexes to global indexes */
-            val gk = DenseVector.zeros[Double](N)
             val translation = rnexpand.map { expandIndex => partition.sampledIndices(expandIndex) }
-  
-            gk(translation) := gk_local
-            gk
-          }
-          .treeReduce { _ + _ }
 
-        α -= (1.0 / iteration) * g
+            val gk = DenseVector.zeros[Double](N)
+            gk(translation) := gk_localAndGhat._1
+
+            val deltaGk = DenseVector.zeros[Double](N)
+            deltaGk(translation) := gk_localAndGhat._2
+
+            (gk, deltaGk)
+          }
+          .treeReduce { case ((gk1, deltaGk1), (gk2, deltaGk2)) => (gk1 + gk2, deltaGk1 + deltaGk2) }
+
+        //α -= (1.0 / iteration) * g
+        α -= invSqrtG(G) :* g
+        
+        G :+= deltaGk
       }
 
       partitions.unpersist()
@@ -105,7 +120,63 @@ object SparkDskl extends App {
 
     α
   }
-  
+
+  def invSqrtG(G: DenseVector[Double]): Vector[Double] = {
+    val invSqrtG = DenseVector.zeros[Double](G.length)
+    var i = 0
+    while (i < invSqrtG.length) {
+      invSqrtG(i) = if (G(i) != 0) { 1.0 / sqrt(G(i)) } else { 0 }
+      i += 1
+    }
+    invSqrtG
+  }
+
+  // def compute_gradient(y,Xpred,Xexpand,w,kernel,C):
+  def gradientAndG(y: Vector[Double], Xpred: Matrix[Double], Xexpand: Matrix[Double], w: Vector[Double],
+               C: Double): (Vector[Double], Vector[Double]) = {
+
+    //K = kernel[0](Xpred,Xexpand,kernel[1])
+    val K = Dskl.gaussianKernel(Xpred.toDenseMatrix, Xexpand.toDenseMatrix, sigma = 1.0)
+
+    //yhat = K.dot(w)
+    val yhat: Vector[Double] = K * w
+
+    //# compute whether or not prediction is in margin
+    //inmargin = (yhat * y) <= 1
+    val yHatTimesY = yhat :* y
+    val yTimesInMargin = DenseVector.zeros[Double](y.length)
+    var i = 0
+    while (i < yHatTimesY.length) {
+      if (yHatTimesY(i) <= 1) {
+        yTimesInMargin(i) = y(i)
+      }
+      i += 1
+    }
+
+    //G = C * w - (y * inmargin).dot(K)
+
+    val CwPerPoint = ((w * C) :/ y.length.toDouble).t
+
+    //(w * C) - yTimesInMarginDotK(0, ::).t
+
+    // in-place updates of K!
+    i = 0
+    while (i < yTimesInMargin.length) {
+      K(i, ::) *= yTimesInMargin(i)
+      K(i, ::) := CwPerPoint - K(i, ::)
+      i += 1
+    }
+
+    //val rowSumOfK = sum(K(::, *))
+    //val gk = Cw - rowSumOfK(0, ::).t
+    val gk = sum(K(::, *))
+
+    val K2 = K :* K
+    val Ghatk = sum(K2(::, *))
+
+    (gk(0, ::).t, Ghatk(0, ::).t)
+  }
+
   def predictSvmEmp(Xexpand: DenseMatrix[Double], Xtarget: DenseMatrix[Double], α: DenseVector[Double]) = {
     Dskl.gaussianKernel(Xexpand, Xtarget, sigma = 1.0) * α
   }
